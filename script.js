99e1469fec5ff80af7c1e5908d80d06c82c04e11
Egor Sizov <mrjarokvk@gmail.com>
2021-01-14 21:59:04 +0300

    все

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
   new Vue({
-    data: {
-      list:[],
-      name: "",
-      surname: "",
-      counter: 0,
+  el: ".form",
+  data: {
+    list:[],
+    name: "",
+    surname: "",
+    counter: 0,
+  },
+  methods: {
+    StudentsList(){
+      const name = this.name.charAt(0).toUpperCase() + this.name.substring(1).toLowerCase()
+      const surname = this.surname.charAt(0).toUpperCase() + this.surname.substring(1).toLowerCase()
+      const student = surname + " " + name
+      this.list.push(student)
+      this.name = ""
+      this.surname = ""
     }
-
-
-
-
-
-
-  })
+  },
+  computed:{
+    zero(){
+      return this.surname.length === 0 || this.name.length === 0;
+    }
+  },
+})
